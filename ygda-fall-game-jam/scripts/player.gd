055ace48aedc0c9ab87c648cca5ae0ad74d71af8
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2025-11-01 10:53:48 -0400

    added a bit of the attack system

--- ygda-fall-game-jam/scripts/player.gd
+++ ygda-fall-game-jam/scripts/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
-@onready var hunter_animated_sprite: AnimatedSprite2D = $HunterAnimatedSprite
-@onready var knight_animated_sprite: AnimatedSprite2D = $KnightAnimatedSprite
+@onready var hunter_sprite: AnimatedSprite2D = $HunterSprite
+@onready var knight_sprite: AnimatedSprite2D = $KnightSprite
 @onready var player_collision: CollisionShape2D = $PlayerCollision
 @onready var attack_cooldown: Timer = $AttackCooldown
 @onready var movement_ability_cooldown: Timer = $MovementAbilityCooldown
 @onready var timer: Timer = $Timer
 @onready var movement_ability_in_action: Timer = $MovementAbilityInAction
+@onready var hunter_weapon_sprite: AnimatedSprite2D = $HunterWeaponSprite
+@onready var knight_weapon_sprite: AnimatedSprite2D = $KnightWeaponSprite
 
 const BASE_MOVEMENT_SPEED: int = 30
 
-var current_animated_sprite: AnimatedSprite2D
+var current_sprite: AnimatedSprite2D
+var current_weapon: AnimatedSprite2D
 var current_character: Character
 var movement_speed: int
 
@@ -61,10 +64,10 @@ var character_data = {}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	character_data = {
-	Character.HUNTER: [MovementSpeed.NORMAL, AttackCooldownLength.NORMAL, MovementAbilityCooldownLength.NORMAL, hunter_animated_sprite],
-	Character.KNIGHT: [MovementSpeed.NORMAL, AttackCooldownLength.NORMAL, MovementAbilityCooldownLength.NORMAL, knight_animated_sprite]
+	Character.HUNTER: [MovementSpeed.NORMAL, AttackCooldownLength.NORMAL, MovementAbilityCooldownLength.NORMAL, hunter_sprite, hunter_weapon_sprite],
+	Character.KNIGHT: [MovementSpeed.NORMAL, AttackCooldownLength.NORMAL, MovementAbilityCooldownLength.NORMAL, knight_sprite, knight_weapon_sprite]
 	}
-	current_animated_sprite = hunter_animated_sprite
+	current_sprite = hunter_sprite
 	set_character(Character.HUNTER)
 	play_animation("down")
 
@@ -77,27 +80,33 @@ func set_attack_cooldown(character: Character) -> void:
 func set_movement_ability_cooldown(character: Character) -> void:
 	movement_ability_cooldown.wait_time = movement_ability_cooldown_length_to_number[character_data[character][2]]
 	
-func set_animated_sprite(new_animated_sprite: Character) -> void:
+func set_sprite(new_sprite: Character) -> void:
 	for character in character_data:
-		character_data[character][3].visible = character == new_animated_sprite
-	current_animated_sprite = character_data[new_animated_sprite][3]
-	
+		character_data[character][3].visible = character == new_sprite
+	current_sprite = character_data[new_sprite][3]
+
+func set_weapon(new_weapon: Character) -> void:
+	for character in character_data:
+		character_data[character][4].visible = character == new_weapon
+	current_weapon = character_data[new_weapon][4]
+
 func set_character(character: Character) -> void:
 	current_character = character
 	attack_cooldown.stop()
 	movement_ability_cooldown.stop()
-	var old_animation = current_animated_sprite.animation
+	var old_animation = current_sprite.animation
 	set_movement_speed(character)
 	set_attack_cooldown(character)
 	set_movement_ability_cooldown(character)
-	set_animated_sprite(character)
+	set_sprite(character)
+	set_weapon(character)
 	play_animation(old_animation)
 	
 	
-func play_animation(animation: String):
-	current_animated_sprite.play(animation)
+func play_animation(animation: String) -> void:
+	current_sprite.play(animation)
 
-func do_movement_ability(character: Character):
+func do_movement_ability(character: Character) -> void:
 	if character == Character.HUNTER:
 		movement_ability_in_action.start()
 	
@@ -105,17 +114,28 @@ func _on_movement_ability_in_action_timeout() -> void:
 	if current_character == Character.HUNTER:
 		movement_ability_cooldown.start()
 
+func move_weapon_with_mouse() -> void:
+	current_weapon.position.x = 0
+	current_weapon.position.y = 0
+	current_weapon.look_at(get_global_mouse_position())
+	current_weapon.position.x = (10 * cos(current_weapon.rotation))
+	current_weapon.position.y = (5 * sin(current_weapon.rotation))
+
 func _physics_process(delta: float) -> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	
 	if input_direction.y > 0:
 		play_animation("down")
+		current_weapon.z_index = 1
 	elif input_direction.y < 0:
 		play_animation("up")
+		current_weapon.z_index = 0
 	elif input_direction.x > 0:
 		play_animation("right")
+		current_weapon.z_index = 1
 	elif input_direction.x < 0:
 		play_animation("left")
+		current_weapon.z_index = 1
 		
 	if (current_character == Character.HUNTER and not movement_ability_in_action.is_stopped()):
 		velocity += input_direction * delta * movement_speed * BASE_MOVEMENT_SPEED * 2
@@ -124,6 +144,8 @@ func _physics_process(delta: float) -> void:
 
 	velocity *= 0.7
 	
+	
+	move_weapon_with_mouse()
 	if Input.is_action_just_pressed("attack") and attack_cooldown.is_stopped():
 		attack_cooldown.start()
 		print("attack")
