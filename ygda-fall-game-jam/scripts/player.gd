ab85bfa1b63d9e81557b2cb2ef9b224d8d1659e9
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2025-11-01 08:10:11 -0400

    added some logic for player

--- ygda-fall-game-jam/scripts/player.gd
+++ ygda-fall-game-jam/scripts/player.gd
@@ -1,7 +1,17 @@
 extends CharacterBody2D
 
-@onready var player_animated_sprite: AnimatedSprite2D = $PlayerAnimatedSprite
+@onready var hunter_animated_sprite: AnimatedSprite2D = $HunterAnimatedSprite
+@onready var knight_animated_sprite: AnimatedSprite2D = $KnightAnimatedSprite
 @onready var player_collision: CollisionShape2D = $PlayerCollision
+@onready var attack_cooldown: Timer = $AttackCooldown
+@onready var movement_ability_cooldown: Timer = $MovementAbilityCooldown
+@onready var timer: Timer = $Timer
+@onready var movement_ability_in_action: Timer = $MovementAbilityInAction
+
+const BASE_MOVEMENT_SPEED: int = 30
+
+var current_animated_sprite: AnimatedSprite2D
+var current_character: Character
 var movement_speed: int
 
 enum MovementSpeed {
@@ -10,26 +20,116 @@ enum MovementSpeed {
 	SLOW
 }
 
-enum CooldownLength {
+var movement_speed_to_number = {
+	MovementSpeed.FAST: 125,
+	MovementSpeed.NORMAL: 100,
+	MovementSpeed.SLOW: 75,
+}
+
+enum AttackCooldownLength {
 	FAST,
 	NORMAL,
 	SLOW
 }
-	
+
+var attack_cooldown_length_to_number = {
+	AttackCooldownLength.FAST: 1,
+	AttackCooldownLength.NORMAL: 3,
+	AttackCooldownLength.SLOW: 5
+}
+
+
+enum MovementAbilityCooldownLength {
+	FAST,
+	NORMAL,
+	SLOW
+}
+
+var movement_ability_cooldown_length_to_number = {
+	MovementAbilityCooldownLength.FAST: 1,
+	MovementAbilityCooldownLength.NORMAL: 3,
+	MovementAbilityCooldownLength.SLOW: 5
+}
+
+enum Character {
+	HUNTER,
+	KNIGHT
+}
+
+var character_data = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	character_data = {
+	Character.HUNTER: [MovementSpeed.NORMAL, AttackCooldownLength.NORMAL, MovementAbilityCooldownLength.NORMAL, hunter_animated_sprite],
+	Character.KNIGHT: [MovementSpeed.NORMAL, AttackCooldownLength.NORMAL, MovementAbilityCooldownLength.NORMAL, knight_animated_sprite]
+	}
+	current_animated_sprite = hunter_animated_sprite
+	set_character(Character.HUNTER)
+	play_animation("down")
 
-func set_movement_speed(new_movement_speed: MovementSpeed) -> void:
-	if new_movement_speed == MovementSpeed.FAST:
-		movement_speed = 1000
-	elif new_movement_speed == MovementSpeed.NORMAL:
-		movement_speed = 500
-	elif new_movement_speed == MovementSpeed.SLOW:
-		movement_speed = 250
-	else:
-		push_error("Invalid movement speed set.")
+func set_movement_speed(character: Character) -> void:
+	movement_speed = movement_speed_to_number[character_data[character][0]]
+
+func set_attack_cooldown(character: Character) -> void:
+	attack_cooldown.wait_time = attack_cooldown_length_to_number[character_data[character][1]]
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func set_movement_ability_cooldown(character: Character) -> void:
+	movement_ability_cooldown.wait_time = movement_ability_cooldown_length_to_number[character_data[character][2]]
+	
+func set_animated_sprite(new_animated_sprite: Character) -> void:
+	for character in character_data:
+		character_data[character][3].visible = character == new_animated_sprite
+	current_animated_sprite = character_data[new_animated_sprite][3]
+	
+func set_character(character: Character) -> void:
+	current_character = character
+	attack_cooldown.stop()
+	movement_ability_cooldown.stop()
+	var old_animation = current_animated_sprite.animation
+	set_movement_speed(character)
+	set_attack_cooldown(character)
+	set_movement_ability_cooldown(character)
+	set_animated_sprite(character)
+	play_animation(old_animation)
+	
+	
+func play_animation(animation: String):
+	current_animated_sprite.play(animation)
+
+func do_movement_ability(character: Character):
+	if character == Character.HUNTER:
+		movement_ability_in_action.start()
+	
+func _on_movement_ability_in_action_timeout() -> void:
+	if current_character == Character.HUNTER:
+		movement_ability_cooldown.start()
+
 func _physics_process(delta: float) -> void:
-	pass
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	if input_direction.y > 0:
+		play_animation("down")
+	elif input_direction.y < 0:
+		play_animation("up")
+	elif input_direction.x > 0:
+		play_animation("right")
+	elif input_direction.x < 0:
+		play_animation("left")
+		
+	if (current_character == Character.HUNTER and not movement_ability_in_action.is_stopped()):
+		velocity += input_direction * delta * movement_speed * BASE_MOVEMENT_SPEED * 2
+	else:
+		velocity += input_direction * delta * movement_speed * BASE_MOVEMENT_SPEED
+
+	velocity *= 0.7
+	
+	if Input.is_action_just_pressed("attack") and attack_cooldown.is_stopped():
+		attack_cooldown.start()
+		print("attack")
+	
+	if Input.is_action_just_pressed("movement_ability") and movement_ability_cooldown.is_stopped() and movement_ability_in_action.is_stopped():
+		do_movement_ability(current_character)
+		print("movement ability")
+	
+	move_and_slide()
